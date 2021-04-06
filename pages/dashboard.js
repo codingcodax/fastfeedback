9a5c792e2bebd9069442c87e0b649bd607521e6b
Alexis Guzman <alexis12989x@gmail.com>
2021-04-06 12:14:40 -0500

    Set up Dashboard page component

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,19 @@
+import { useContext } from 'react';
+
+import { AuthContext } from '@/context/AuthContext';
+
+import EmptyState from '@/components/EmptyState';
+import SiteTableSkeleton from '@/components/SiteTableSkeleton';
+import DashboardShell from '@/components/DashboradShell';
+
 const Dashboard = () => {
-	return (
-		<div>
-			<p>Dashboard Page Component</p>
-		</div>
-	);
+    const { user } = useContext(AuthContext);
+
+    return (
+        <DashboardShell>
+            {!user ? <SiteTableSkeleton /> : <EmptyState />}
+        </DashboardShell>
+    );
 };
 
 export default Dashboard;
