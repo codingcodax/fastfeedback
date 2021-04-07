59997cd65fbb558e1eabb3ae59862eb01a4d60ef
Alexis Guzman <alexis12989x@gmail.com>
2021-04-07 12:37:32 -0500

    Make AuthContext available to Layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,20 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider } from '@chakra-ui/react';
 
-import Layout from "@/components/Layout";
-import AuthContextProvider from "@/context/AuthContext";
+import Layout from '@/components/Layout';
+import AuthContextProvider from '@/context/AuthContext';
 
-import theme from "@/styles/theme";
+import theme from '@/styles/theme';
 
 const MyApp = ({ Component, pageProps }) => {
-	return (
-		<ChakraProvider resetCSS theme={theme}>
-			<Layout>
-				<AuthContextProvider>
-					<Component {...pageProps} />
-				</AuthContextProvider>
-			</Layout>
-		</ChakraProvider>
-	);
+    return (
+        <ChakraProvider resetCSS theme={theme}>
+            <AuthContextProvider>
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </AuthContextProvider>
+        </ChakraProvider>
+    );
 };
 
 export default MyApp;
