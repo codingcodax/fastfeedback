ed26847f7869af5c8614e060eb96b3f64f5ce9f9
Alexis Guzman <alexis12989x@gmail.com>
2021-04-07 12:37:46 -0500

    Change double with single quotes

--- pages/index.js
+++ pages/index.js
@@ -1,33 +1,33 @@
-import { useContext } from "react";
-import { Flex, Button, ButtonGroup, Heading, Text } from "@chakra-ui/react";
+import { useContext } from 'react';
+import { Flex, Button, ButtonGroup, Heading, Text } from '@chakra-ui/react';
 
-import { AuthContext } from "@/context/AuthContext";
-import { LogoIcon } from "@/styles/theme";
+import { AuthContext } from '@/context/AuthContext';
+import { LogoIcon } from '@/styles/theme';
 
 const Home = () => {
-	const { user, signInWithGithub, signOut } = useContext(AuthContext);
+    const { user, signInWithGithub, signOut } = useContext(AuthContext);
 
-	return (
-		<Flex
-			as="main"
-			direction="column"
-			align="center"
-			justify="center"
-			h="100vh"
-		>
-			<LogoIcon boxSize="64px" />
+    return (
+        <Flex
+            as='main'
+            direction='column'
+            align='center'
+            justify='center'
+            h='100vh'
+        >
+            <LogoIcon boxSize='64px' />
 
-			{!user ? (
-				<Button mt={4} size="sm" onClick={signInWithGithub}>
-					Sign In
-				</Button>
-			) : (
-				<Button mt={4} size="sm" onClick={signOut}>
-					Sign Out
-				</Button>
-			)}
-		</Flex>
-	);
+            {!user ? (
+                <Button mt={4} size='sm' onClick={signInWithGithub}>
+                    Sign In
+                </Button>
+            ) : (
+                <Button mt={4} size='sm' onClick={signOut}>
+                    Sign Out
+                </Button>
+            )}
+        </Flex>
+    );
 };
 
 export default Home;
