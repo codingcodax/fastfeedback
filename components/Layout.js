60fff88cc234e679db69f59c6f34911e62aee178
Alexis Guzman <alexis12989x@gmail.com>
2021-04-07 12:36:01 -0500

    Set up Layout component

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,38 @@
-import Head from "./Head";
+import { useContext } from 'react';
+import { LogoIcon } from '@/styles/theme';
+import { Avatar } from '@chakra-ui/avatar';
+import { Flex, Link, Stack } from '@chakra-ui/layout';
+
+import Head from './Head';
+import { AuthContext } from '@/context/AuthContext';
 
 const Layout = ({ children }) => {
-	return (
-		<>
-			<Head />
-			{children}
-		</>
-	);
+    const { user } = useContext(AuthContext);
+
+    return (
+        <>
+            <Head />
+            <Flex flexDirection='column'>
+                <Flex justify='space-between' align='center' py={4} px={8}>
+                    <Stack spacing={4} isInline align='center'>
+                        <LogoIcon boxSize='24px' />
+                        <Link>Feedback</Link>
+                        <Link>Sites</Link>
+                    </Stack>
+                    <Stack
+                        spacing={4}
+                        isInline
+                        justify='flex-end'
+                        align='center'
+                    >
+                        <Link>Account</Link>
+                        <Avatar size='sm' src={user?.photoURL} />
+                    </Stack>
+                </Flex>
+                {children}
+            </Flex>
+        </>
+    );
 };
 
 export default Layout;
