61d9cc662571f71d926b1e62e5b4c6832d439216
Alexis Guzman <alexis12989x@gmail.com>
2021-04-08 17:29:08 -0500

    Refact function with try catch

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -1,7 +1,11 @@
 import { getSAllSites } from '@/lib/firebase';
 
 export default async (_, res) => {
-    const sites = await getSAllSites();
+    try {
+        const sites = await getSAllSites();
 
-    res.status(200).json(sites);
+        res.status(200).json(sites);
+    } catch (err) {
+        res.status(500).json({ error: err });
+    }
 };
