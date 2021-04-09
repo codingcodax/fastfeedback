eda6744597fbd69124886b2a1273dc87a169fb56
Alexis Guzman <alexis12989x@gmail.com>
2021-04-09 11:48:51 -0500

    Create sign in/out function for add cookie and remove

--- context/AuthContext.js
+++ context/AuthContext.js
@@ -2,15 +2,32 @@ import { createContext } from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 
 import { auth, signInWithGithub, signOut } from '@/lib/firebase';
+import Cookies from 'js-cookie';
 
 export const AuthContext = createContext();
 
 const AuthContextProvider = ({ children }) => {
     const [user, loading, error] = useAuthState(auth);
 
+    const signInWithGithubContext = () => {
+        signInWithGithub();
+        Cookies.set('fast-feedback-auth', true, { expires: 1 });
+    };
+
+    const signOutContext = () => {
+        signOut();
+        Cookies.remove('fast-feedback-auth');
+    };
+
     return (
         <AuthContext.Provider
-            value={{ user, loading, error, signInWithGithub, signOut }}
+            value={{
+                user,
+                loading,
+                error,
+                signInWithGithubContext,
+                signOutContext,
+            }}
         >
             {children}
         </AuthContext.Provider>
