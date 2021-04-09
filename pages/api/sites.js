e1f218a6984bc2f7b6e0f24aa8fe57ef48c951fb
Alexis Guzman <alexis12989x@gmail.com>
2021-04-09 12:03:19 -0500

    Remove console log

--- pages/api/sites.js
+++ pages/api/sites.js
@@ -4,7 +4,6 @@ import { auth } from '@/lib/firebase-admin';
 export default async ({ headers: { token } }, res) => {
     try {
         const { uid } = await auth.verifyIdToken(token);
-        console.log(uid);
         const sites = await getUserSites(uid);
 
         res.status(200).json(sites);
