b83d8f3c332ea339a002ded282776e19821f7fa0
Alexis Guzman <alexis12989x@gmail.com>
2021-04-09 11:44:45 -0500

    Create function for fetch with token and export

--- utils/fetcher.js
+++ utils/fetcher.js
@@ -1,3 +1,12 @@
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+export const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
-export default fetcher;
+export const fetchWithToken = async (url, token) => {
+    const res = await fetch(url, {
+        method: 'GET',
+        headers: new Headers({ 'Content-Type': 'application/json', token }),
+        credentials: 'same-origin',
+    });
+    const data = await res.json();
+
+    return data;
+};
