89a48a8f2fa62af13a1a4f9286637cb8a8153376
Alexis Guzman <alexis12989x@gmail.com>
2021-04-10 10:05:46 -0500

    Set up DashboardHeader component

--- components/DashboardHeader.js
+++ components/DashboardHeader.js
@@ -1,8 +1,27 @@
-const DashboardHeader = () => {
+import {
+    Breadcrumb,
+    BreadcrumbItem,
+    BreadcrumbLink,
+} from '@chakra-ui/breadcrumb';
+import { Flex, Heading } from '@chakra-ui/layout';
+
+import AddSiteModal from './AddSiteModal';
+
+const DashboardHeader = ({ heading, breadcrumbLink, data }) => {
     return (
-        <div>
-            <p>DashboardHeader Component</p>
-        </div>
+        <>
+            <Breadcrumb mb={2}>
+                <BreadcrumbItem isCurrentPage>
+                    <BreadcrumbLink color='gray.700' fontSize='sm'>
+                        {breadcrumbLink}
+                    </BreadcrumbLink>
+                </BreadcrumbItem>
+            </Breadcrumb>
+            <Flex mb={4} justifyContent='space-between'>
+                <Heading size='2xl'>{heading}</Heading>
+                {data && <AddSiteModal dashboard />}
+            </Flex>
+        </>
     );
 };
 
