f0bf5662fc8f598b56c71ef853c06c83116786ae
Alexis Guzman <alexis12989x@gmail.com>
2021-04-12 12:36:43 -0500

    Fix strokeWidth property

--- styles/theme.js
+++ styles/theme.js
@@ -36,7 +36,7 @@ export const GithubIcon = (props) => (
         xmlns='http://www.w3.org/2000/svg'
         fill='none'
         stroke='currentColor'
-        stroke-width='2'
+        strokeWidth='2'
         viewBox='0 0 24 24'
         {...props}
     >
