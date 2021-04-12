56241cfcce9dafd3ca9b33bdeb2adb92ace76ce3
Alexis Guzman <alexis12989x@gmail.com>
2021-04-12 10:45:41 -0500

    Set up Header component

--- components/Header.js
+++ components/Header.js
@@ -1,8 +1,43 @@
+import NextLink from 'next/link';
+import { LogoIcon } from '@/styles/theme';
+import { Flex, Link, Stack } from '@chakra-ui/layout';
+import { useContext } from 'react';
+import { AuthContext } from '@/context/AuthContext';
+import { Avatar } from '@chakra-ui/avatar';
+
 const Header = () => {
+    const { user } = useContext(AuthContext);
+
     return (
-        <div>
-            <p>Header Component</p>
-        </div>
+        <Flex
+            py={4}
+            px={8}
+            justify='space-between'
+            align='center'
+            backgroundColor='white'
+        >
+            <Stack spacing={4} isInline align='center'>
+                <NextLink href={`${user ? '/sites' : '/'}`} passHref>
+                    <Link>
+                        <LogoIcon boxSize='24px' />
+                    </Link>
+                </NextLink>
+                <NextLink href='/sites' passHref>
+                    <Link>Sites</Link>
+                </NextLink>
+                <NextLink href='/feedback' passHref>
+                    <Link>Feedback</Link>
+                </NextLink>
+            </Stack>
+
+            {user && (
+                <NextLink href='/account' passHref>
+                    <Link>
+                        <Avatar size='sm' src={user?.photoURL} />
+                    </Link>
+                </NextLink>
+            )}
+        </Flex>
     );
 };
 
